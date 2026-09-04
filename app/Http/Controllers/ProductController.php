e335518662dd25d4e47b6fb1a44b6d1f466f8cb5
Dmitry <dmper67@gmail.com>
2026-09-04 18:03:49 +0300

    Добавлена возможность фильтрации

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,10 +7,20 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
-    {
-        return Product::with(['type', 'condition'])->get();
+   public function index(Request $request)
+{
+    $query = Product::with(['type', 'condition']);
+
+    if ($request->has('type_id') && $request->type_id != '') {
+        $query->where('type_id', $request->type_id);
     }
+
+    if ($request->has('condition_id') && $request->condition_id != '') {
+        $query->where('condition_id', $request->condition_id);
+    }
+
+    return $query->get();
+}
      // Один товар
     public function show($id)
     {
